b6ab0d49486b9609d121e9c7d16f7c00d49b1f19
skezzy1 <serhiy1204@gmail.com>
2022-10-02 23:15:58 +0300

    init commit

--- 10/с_10/с_10/с_10.cpp
+++ 10/с_10/с_10/с_10.cpp
@@ -1,10 +1,17 @@
-#include <iostream>
-using namespace std;
+//Програма на вході приймає натуральне число m – вага в грамах.Потрібно вивести тільки повні кілограми цієї ваги
 
-int main() {
-	int x, n = 1;
-	cin >> x;
-	while ((x /= 10) > 0) n++;
-	cout << n;
+#include <iostream> 
+#include <cmath> 
+using namespace std;
+int main()
+{
+	int m, b, k, l;
+	float a;
+	cin >> m;
+	k = m / 1000;
+	a = int(k);
+	l = m % 1000;
+	b = l;
+	cout << a << " " << endl;
 	return 0;
 }
