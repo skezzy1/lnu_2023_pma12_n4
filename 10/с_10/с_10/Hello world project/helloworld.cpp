23f0a0a5f0707b8f5d8dddeb18dc8210a832d7bb
skezzy1 <serhiy1204@gmail.com>
2022-10-02 20:45:59 +0300

    int commit

--- 10/с_10/с_10/Hello world project/helloworld.cpp
+++ 10/с_10/с_10/Hello world project/helloworld.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main()
-{
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
+{   
+    int a, b, c, d, f, task;
+    cout << "Press A: " << endl;
+    cin >> a;
+    cout << "Press B: " << endl;
+    cin >> b;
+    cout << "Press C: " << endl;
+    cin >> c;
+    cout << "Press D: " << endl;
+    cin >> d;
+    cout << "Press F: " << endl;
+    cin >> f;
+    task = a + b + c + d + f / 5;
+    cout << "Result: " << task << endl;
+    return 0;
 }
--- 10/с_10/с_10/с_10.cpp
+++ 10/с_10/с_10/с_10.cpp
@@ -1,8 +1,20 @@
-﻿#include <iostream>
+﻿#include<iostream>
 using namespace std;
+
 int main()
 {
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
+    int n;
+    cin >> n;
+    cout << "Enter a number: ";
+    for (int i = 0; i < n; i++)
+    {
+        if (n > n = pow(10,6)) {
+            cout << "Limit is over";
+        }else if(n == 0) {
+            cout << "Null is not natural number";
+        }else {
+            cout << "Correct result" << n << endl;
+        }
+    }
+    return 0;
 }
-
