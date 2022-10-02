2c1c86463ccf8dc640cc71356428c70b20b317f3
skezzy1 <serhiy1204@gmail.com>
2022-10-02 21:38:39 +0300

    int commit

--- 10/с_10/с_10/с_10.cpp
+++ 10/с_10/с_10/с_10.cpp
@@ -1,20 +1,10 @@
-﻿#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
-    cout << "Enter a number: ";
-    for (int i = 0; i < n; i++)
-    {
-        if (n > n = pow(10,6)) {
-            cout << "Limit is over";
-        }else if(n == 0) {
-            cout << "Null is not natural number";
-        }else {
-            cout << "Correct result" << n << endl;
-        }
-    }
-    return 0;
+int main() {
+	int x, n = 1;
+	cin >> x;
+	while ((x /= 10) > 0) n++;
+	cout << n;
+	return 0;
 }
