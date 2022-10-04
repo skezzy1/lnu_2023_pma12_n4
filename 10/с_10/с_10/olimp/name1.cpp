f00a3d328c99fd81979cf3767294085012d0b704
skezzy1 <serhiy1204@gmail.com>
2022-10-04 21:28:51 +0300

    init commit

--- 10/с_10/с_10/olimp/name1.cpp
+++ 10/с_10/с_10/olimp/name1.cpp
@@ -1,14 +1,16 @@
+/*https://www.eolymp.com/uk/problems/271 (не виконано)*/
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 int main() {
-	unsigned int n;
+	unsiged int n;
 	cout << "Enter N: ";
 	cin >> n;
-	for (int i = 1 ; i != 10 ; i++) {
-    print("%lld %f\n", factorial(i), tgamma(i+1));
-}	
+	long int f = 1;
+	for (int i = 2; i <= n; i++) {
+		f = f * i;
+		cout << f;
+	}
+	return 0;
 }
-
-
