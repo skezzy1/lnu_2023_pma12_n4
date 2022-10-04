d5420c411795cd9cacc0ffa9ee557ae73ef720f5
skezzy1 <serhiy1204@gmail.com>
2022-10-04 21:26:15 +0300

    init commit

---
+++ 10/с_10/с_10/Hello world/helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	cout << "Hello word\n";
+	cout << "Hello word\n";
+}
---
+++ 10/с_10/с_10/olimp/name1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	unsigned int n;
+	cout << "Enter N: ";
+	cin >> n;
+	for (int i = 1 ; i != 10 ; i++) {
+    print("%lld %f\n", factorial(i), tgamma(i+1));
+}	
+}
+
+
