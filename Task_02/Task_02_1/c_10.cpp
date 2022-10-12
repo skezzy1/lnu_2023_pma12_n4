0db811e1cb68461f42a4c925a5c997a2af8278c5
skezzy1 <serhiy1204@gmail.com>
2022-10-12 09:54:55 +0300

    init commit

--- Task_02/Task_02_1/c_10.cpp
+++ Task_02/Task_02_1/c_10.cpp
@@ -2,21 +2,34 @@
 #include <cmath>
 using namespace std;
 
-int main() {
-	int a, b, h;
-	float x, y, g = x * y, max = 0, min = 0;
-	cout << "Enter A(from): ";
-	cin >> a;
-	cout << "Enter B(to): ";
-	cin >> b;
-	cout << "Enter h(steps): ";
-	cin >> h;
-	for (x = a; x <= b; x += h) {
-		y = x * sin(x);
-		cout << x << "\t\t\t\t\t" << y << endl;
-		if (y > max) { max = y;}
-	}
-	cout << "Max: " << max << endl;
+int main()
+{
+    float a, b, h;
+    cout << "Enter a, b and h: ";
+    cin >> a >> b >> h;
+    float x = a;
+    float y, sum = 0;
+    int count = 0;
+    int n=0;
+    float max = 0;
+    y = max;
+    while (x <= b) {
+        if (h <= 0) {
+            cout << "Please enter a bigger than null number: ";
+            break;
+        }
+        y = x * sin(x);
+        cout << "x = " << x << "; y = " << y << endl;
+        if (y < 0) {
+            count += 1;
+            count++;
+        }
+        if (max < y) {
+            max = y;
+        }
+        x = x + h;
+    }
+    cout << "count y < 0: " << count << endl;
+    cout << "The maximum value of the function: " << y << endl;
+    return 0;
 }
-
-
